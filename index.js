39cdbd8b0edafbf52fa8c43f95e6317a742fcc6c
namanTiwari <er.amantiwari.2002@gmail.com>
2023-04-03 11:00:02 +0530

    Successfully Connected to the database

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // Importing required libraries
+const { urlencoded } = require('body-parser');
 const express = require('express');
 const port = 8000;
+
+// Connecting the database 
+const db = require('./config/mongoose');
 
 // Initialising Express
 const app =express();
@@ -8,6 +12,12 @@ const app =express();
 // Setting up the View Engine :EJS 
 app.set('view engine','ejs');
 app.set('views','./views');
+
+// Using Middleware to decode the URL Encoded sent by the user 
+app.use(urlencoded());
+
+// Using the static files 
+app.use(express.static('asset'));
 
 // Using Middleware to handle Get Request and directing them to Routes 
 app.use('/',require('./routes/index'));
